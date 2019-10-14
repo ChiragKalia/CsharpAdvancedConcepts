14a78d5c81b490b5a1895efb9a1e45035b858fd5
Chirag Kalia <chiragkalia1@gmail.com>
2019-10-15 01:30:32 +0530

    Checked In Lambda Expressions
    Signed-off-by: Chirag Kalia <chiragkalia1@gmail.com>

---
+++ Lambda Expressions/BookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaExpressions
+{
+    class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book(){ Title = "Book 1", Price = 13},
+                new Book(){ Title = "Book 2", Price = 19}
+            };
+        }
+    }
+}
---
+++ Lambda Expressions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // args => expression;
+            // number => number*number;
+            Func<int, int> square = number => number * number;
+            Console.WriteLine(square(5));
+        }
+    }
+}
