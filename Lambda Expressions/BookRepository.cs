c0ba5c9c92e65e14dd4e92e4c5b0f52664f1eee3
Chirag Kalia <chiragkalia1@gmail.com>
2019-10-16 23:03:19 +0530

    Added Lambda Expressions Chapter

--- Lambda Expressions/BookRepository.cs
+++ Lambda Expressions/BookRepository.cs
@@ -4,14 +4,15 @@ using System.Text;
 
 namespace LambdaExpressions
 {
-    class BookRepository
+    public class BookRepository
     {
         public List<Book> GetBooks()
         {
             return new List<Book>
             {
                 new Book(){ Title = "Book 1", Price = 13},
-                new Book(){ Title = "Book 2", Price = 19}
+                new Book(){ Title = "Book 2", Price = 19},
+                new Book(){ Title = "Book 3", Price = 21}
             };
         }
     }
--- Lambda Expressions/Program.cs
+++ Lambda Expressions/Program.cs
@@ -6,10 +6,13 @@ namespace LambdaExpressions
     {
         static void Main(string[] args)
         {
-            // args => expression;
-            // number => number*number;
-            Func<int, int> square = number => number * number;
-            Console.WriteLine(square(5));
+            var books = new BookRepository().GetBooks();
+            var cheaperBooks = books.FindAll(x => x.Price > 10); // Using LE
+
+            foreach (var book in cheaperBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
         }
     }
 }
