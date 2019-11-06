571341249d5b77048da95b334a42854120131715
Chirag Kalia <chiragkalia1@gmail.com>
2019-11-06 18:07:56 +0530

    Added Implementation of Extension Methods
    Change-Id: I5e34aaf46936fb94d54def6ff486e27aa6509967

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long post blah blah blah...";
+            var shorterPost = post.Shorten(5);
+            Console.WriteLine(shorterPost);
+
+            //IEnumerable<int> numbers = new List<int>() {1,3,5,8,14,4,25 };
+            //Console.WriteLine(numbers.Max());  //Max() is an inbuilt LINQ Extension method.
+        }
+    }
+}
---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System //ExtensionMethods //In order to make this extension method available everywhere
+// We should use the System namespace instead
+{
+    //According to C# convention, Extension class should be public and static
+    //And it should begin with the name of the class which we are extending.
+    //Microsoft says that Extension methods should be used only when required ->
+    //Because if microsoft decides to create Shorten method in the String Class
+    //with the same syntax then it will cause errors.
+    public static class StringExtensions
+    {
+        public static string Shorten(this string str, int numberOfWords)
+        {
+            if (numberOfWords <= 0)
+                throw new InvalidOperationException();
+            var words = str.Split(' ');
+            if (words.Length <= numberOfWords)
+                return str; //Return the original string if there's only one word.
+            return String.Join(" ", words.Take(numberOfWords)) + "...";
+        }
+    }
+}
