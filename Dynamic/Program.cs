3f38f6d293f11cf810064a570fc3101b22fd54dd
Chirag Kalia <chiragkalia1@gmail.com>
2019-11-07 12:53:40 +0530

    Implemented Dynamic in C#
    Change-Id: I3cbef8b33e7da8d1b9b55df6fce64498614c5e32

---
+++ Dynamic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "Kalia";
+            //obj.GetHashCode(); 
+
+            //Using Reflection
+            //We can see this code is not beautiful as reflection is difficult and messy.
+
+            var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            methodInfo.Invoke(null, null);
+
+            //Using Dynamic
+            //We can see that using dynamic, Our code is much cleaner.
+            //Type resolving happens via DLR(Dynamic Language Runtime) which
+            //Sits on top of CLR and gives Dynamic language capabilities to C#
+
+            dynamic excelObject = "Kalia";
+            excelObject.Optimize(); //Doesn't exist but doesn't throw a compile time error.
+
+            //Using Dynamic, we can also change the type of an already assigned variable
+            //For example
+
+            dynamic d = "Kalia";
+            d = 10;
+            d++;
+            Console.WriteLine(d);
+        }
+    }
+}
